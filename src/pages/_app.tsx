d920fe4a8154bcd545943afb54a86b5ba366e55a
jeffreyquan <50325482+jeffreyquan@users.noreply.github.com>
2022-04-20 01:42:36 +1000

    feat: install and setup mantine (#3)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,32 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 import { useApollo } from "../lib/apolloClient";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps);
   return (
     <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "light",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </ApolloProvider>
   );
 }
